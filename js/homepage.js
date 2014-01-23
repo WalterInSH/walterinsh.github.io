3c6f3c9caa744ffdfb2e08a78e74f0b6b950982b
Walter <coolwaterever@gmail.com>
2014-01-23 17:50:42 +0800

    add svg

--- js/homepage.js
+++ js/homepage.js
@@ -1,8 +1,23 @@
-var big_bar_color = ['7B3CBD','8E0FF4','9B43F4','AA7BF0'];
+/*var big_bar_color = ['7B3CBD','8E0FF4','9B43F4','AA7BF0'];
 
 $(function(){
 	$("#posts>.big_bar").each(function(index){
 		console.log(index);
 		$(this).css({"background-color":"#"+big_bar_color[index]});
 	});
-});
+});*/
+
+var width = $("#posts").width();
+
+var chart = d3.select("#posts svg")
+	.attr("width",width)
+	.attr("height",500);
+
+var bar = chart.selectAll("g")
+	.data(blogData)
+	.enter().append("g")
+	.attr("transform", function(data,i) { return "translate("+ i * 30 +",0)"; });
+
+bar.append("rect")
+    .attr("width", 3)
+    .attr("height", 40);
