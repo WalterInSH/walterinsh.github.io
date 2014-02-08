79dd20d7fc447eba307ff6aff984a47a67add4cb
Walter <coolwaterever@gmail.com>
2014-02-08 18:00:45 +0800

    simplify posts list

--- js/homepage.js
+++ js/homepage.js
@@ -1,8 +1,8 @@
 var big_bar_color = ['7B3CBD','8E0FF4','9B43F4','AA7BF0'];
 
-$(function(){
+/*$(function(){
 	$("#posts>.big_bar").each(function(index){
 		console.log(index);
 		$(this).css({"background-color":"#"+big_bar_color[index]});
 	});
-});
+});*/
