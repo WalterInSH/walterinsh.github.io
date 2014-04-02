654b6770e4cdd98a505da3ba3fa111c80515da55
Walter <coolwaterever@gmail.com>
2014-04-02 16:06:58 +0800

    move tags into same js file to improve load speed

--- js/homepage.js
+++ js/homepage.js
@@ -1,15 +1,15 @@
-$.get( "/tags.json", function( data ) {
-    // add tags
-    $.each(data, function (index,tag) {
-        var tag_content = "<span class=\"tag\">" + tag + "</span>";
-        $(tag_content).hide().appendTo("#blog_tags").fadeIn(1500);
-    });
+var tags = ["Maven", "Test", "Java", "Spring", "MyBatis", "Vim", "HTTP", "Web", "Heritrix"];
 
-    //bind click event onto tags
-    $("#blog_tags .tag").each(function(){
-        var tag = $(this).text();
-        $(this).bind("click", function () {
-            window.location = "https://www.google.com/search?q=site:faolou.me" + encodeURIComponent(" "+tag);
-        });
+// add tags
+$.each(tags, function (index, tag) {
+    var tag_content = "<span class=\"tag\">" + tag + "</span>";
+    $(tag_content).hide().appendTo("#blog_tags").fadeIn(1500);
+});
+
+//bind click event onto tags
+$("#blog_tags .tag").each(function () {
+    var tag = $(this).text();
+    $(this).bind("click", function () {
+        window.location = "https://www.google.com/search?q=site:faolou.me" + encodeURIComponent(" " + tag);
     });
 });
